2f793d863130ecf46c85a7d1991c353853969f70
madsBA <madsboeandersen03@gmail.com>
2023-12-04 14:36:51 +0100

    Ballplay
    Indsatte p5.play og begyndte på selve spillepladen (eksternt)

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let polet
+let bund
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(450, 400);
+  polet = new Sprite();
+  polet.diameter = 60;
+  bund = new Sprite(width/2,height-2,'s');
+  bund.width = width;
+  bund.height = 3;
+
+  world.gravity.y = 3
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+}
+
+function mousePressed() {
 }
