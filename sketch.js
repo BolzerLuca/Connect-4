b3b39289e90e5f83d75f772f1501898c1996f9fb
madsBA <madsboeandersen03@gmail.com>
2023-12-07 10:42:20 +0100

    Søjler

--- sketch.js
+++ sketch.js
@@ -1,13 +1,40 @@
 let polet
+let polet2
+let polet3
+let polet4
+let polet5
+let polet6
+let polet7
 let bund
 
 function setup() {
-  createCanvas(450, 400);
+  createCanvas(450, 450);
   polet = new Sprite();
-  polet.diameter = 60;
+  polet.diameter = 50;
+  polet.stroke = ('white');
+  polet2 = new Sprite ();
+  polet2.diameter = 50;
+  polet3 = new Sprite ();
+  polet3.diameter = 50;
+  polet4 = new Sprite ();
+  polet4.diameter = 50;
+  polet5 = new Sprite ();
+  polet5.diameter = 50;
+  polet6 = new Sprite ();
+  polet6.diameter = 50;
+  polet7 = new Sprite ();
+  polet7.diameter = 50;
   bund = new Sprite(width/2,height-2,'s');
   bund.width = width;
-  bund.height = 3;
+  bund.height = 9;
+  bund.color = ('darkblue')
+  bund.stroke = ('darkblue')
+
+  for (let i = 0; i < 8; i++){
+    søjle = new Sprite(0+(64*i),300 ,9 , 390,"s");
+    søjle.color = ("darkblue")
+    søjle.stroke = ('darkblue')
+  }
 
   world.gravity.y = 3
 }
